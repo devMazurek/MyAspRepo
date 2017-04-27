f13e9511b06533b5876da1e0bf6edb7754f29f40
Przemysław Mazurek <przemyslaw.mazurek@outlook.com>
2017-04-27 19:36:30 +0200

    V1.03

--- AHP2/AHP2/Models/User.cs
+++ AHP2/AHP2/Models/User.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.Design;
 
 namespace AHP2.Models
 {
     public class User
     {
+        [Key]
         public int Id { set; get; }
 
         [Display(Name = "Your email")]
-        [EmailAddress, Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Not valid email address")]
+        [Required(ErrorMessage = "Email is required")]
+        [Index("EmailAddressIndex", IsUnique = true)]
+        [Column(TypeName = "VARCHAR")]
+        [MaxLength(50)]
         public string EmailAdress { set; get; }
 
-        [Required(ErrorMessage = "Password is required. Minimum 6 charakters."), MinLength(6)]
+        [Required(ErrorMessage = "Password is required. Minimum 6 charakters."), MinLength(6, ErrorMessage = "Minimum 6 charakters")]
         [DataType(DataType.Password)]
         public string Password { set; get; }
 
